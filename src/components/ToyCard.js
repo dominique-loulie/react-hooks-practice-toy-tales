fe2edb60007a11753d87790d827e631f8264e340
Dominique Loulie <dom.loulie@gmail.com>
2023-07-16 15:13:35 +1000

    this is my first

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, handleDelete, handleClickLikes }) {
+  const { id, image, name, likes } = toy;
+  
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
-      <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
-        className="toy-avatar"
-      />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <h2>{name}</h2>
+      <img src={image} alt={name} className="toy-avatar" />
+      <p>{likes} Likes </p>
+      <button onClick={() => handleClickLikes(toy)} className="like-btn">Like {"<3"}</button>
+      <button onClick={() => handleDelete(id)} className="del-btn">Donate to GoodWill</button>
     </div>
   );
 }
